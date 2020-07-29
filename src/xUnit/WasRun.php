0b6e64bf95f8f7caf621e846644d0d5308eed62f
Kentaro Minami <mkentaro555@gmail.com>
2020-07-29 18:24:00 +0900

    testMethodが走ったかどうかを確認できるように、wasRunに0(走る前)と1(走った後)の記録メソッドを追加。

---
+++ src/xUnit/WasRun.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\xUnit;
+
+class WasRun
+{
+    public $wasRun;
+    private $name;
+
+    public function __construct(String $name)
+    {
+        $this->wasRun = 0;
+        $this->name = $name;
+    }
+
+    public function run()
+    {
+        $this->testMethod();
+    }
+
+    public function testMethod()
+    {
+        $this->wasRun = 1;
+    }
+}
